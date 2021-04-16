2f651722e03f060e8164992f28c024acaadcdfdf
Habyb Fernandes <habybfernandes@gmail.com>
2021-04-16 05:27:57 -0300

    import files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home/home.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -14,65 +16,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepOrange,
       ),
       home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return _buildHome();
-  }
-
-  Widget _buildHome() {
-    return Scaffold(
-      body: _buildCard(),
-      appBar: _buildAppBar(),
-    );
-  }
-
-  Widget _buildCard() {
-    return SizedBox(
-      height: 300,
-      child: Card(
-        margin: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                _buildCardImage(),
-                _buidCardText(),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Image _buildCardImage() {
-    return Image.network(
-      'https://milkandbun.files.wordpress.com/2015/01/orange-cake-1.jpg',
-      fit: BoxFit.fill,
-      height: 268,
-    );
-  }
-
-  Positioned _buidCardText() {
-    return Positioned(
-      bottom: 10,
-      left: 10,
-      child: Text(
-        'Orange Cake',
-        style: TextStyle(fontSize: 20),
-      ),
-    );
-  }
-
-  AppBar _buildAppBar() {
-    return AppBar(
-      title: Text('Cooking at home'),
-      centerTitle: true,
     );
   }
 }
